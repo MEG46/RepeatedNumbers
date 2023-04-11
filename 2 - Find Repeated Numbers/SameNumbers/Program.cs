9dd77cbbc538d98e2782dde20d6e54a9b014f248
Muhammed Esad Goncaloğlu <93635211+MEG46@users.noreply.github.com>
2023-04-11 07:31:28 +0300

    Add files via upload

---
+++ 2 - Find Repeated Numbers/SameNumbers/Program.cs
@@ -0,0 +1,37 @@
+﻿int num,i,j=0,temp;
+Console.Write("Please enter how many elements do you want to add your array : ");
+num = int.Parse(Console.ReadLine());
+int []arr = new int[num];
+for (i = 0; i < num; i++)
+{
+    Console.Write((i + 1) + ".Element : ");
+    arr[i] = int.Parse(Console.ReadLine());
+
+}
+Console.WriteLine("The Array:");
+for (i = 0; i < num; i++)
+{
+    Console.Write(arr[i]+"\t");
+
+}
+Console.WriteLine("\nThe Repeated Elements:");
+while (num > j)
+{
+    temp = arr[j];
+    for (i = 0; i < num; i++)
+    {
+        if (j == i)
+        {
+            break;
+        }
+        if (temp == arr[i])
+        {
+            Console.Write(temp + "\t");
+        }
+
+    }
+    j++;
+}
+
+
+
